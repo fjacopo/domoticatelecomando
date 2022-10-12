f02bc39dd74deaf62fbec3c38001df5db580f9f9
fjacopo <fjacopo10@gmail.com>
2022-10-12 10:27:05 +0200

    pt2

--- 10-10-2022/Form1.Designer.cs
+++ 10-10-2022/Form1.Designer.cs
@@ -40,9 +40,10 @@
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.Red;
-            this.button1.Location = new System.Drawing.Point(145, 53);
+            this.button1.Location = new System.Drawing.Point(193, 65);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(100, 28);
             this.button1.TabIndex = 0;
             this.button1.Text = "OFF";
             this.button1.UseVisualStyleBackColor = false;
@@ -51,9 +52,10 @@
             // button2
             // 
             this.button2.BackColor = System.Drawing.Color.LawnGreen;
-            this.button2.Location = new System.Drawing.Point(32, 53);
+            this.button2.Location = new System.Drawing.Point(43, 65);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(100, 28);
             this.button2.TabIndex = 1;
             this.button2.Text = "ON";
             this.button2.UseVisualStyleBackColor = false;
@@ -61,54 +63,63 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(32, 124);
+            this.button3.Location = new System.Drawing.Point(43, 153);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.Size = new System.Drawing.Size(100, 28);
             this.button3.TabIndex = 2;
             this.button3.Text = "Vol +";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(32, 153);
+            this.button4.Location = new System.Drawing.Point(43, 188);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.Size = new System.Drawing.Size(100, 28);
             this.button4.TabIndex = 3;
             this.button4.Text = "Vol -";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(145, 124);
+            this.button5.Location = new System.Drawing.Point(193, 153);
+            this.button5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.Size = new System.Drawing.Size(100, 28);
             this.button5.TabIndex = 4;
             this.button5.Text = "Can +";
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(145, 153);
+            this.button6.Location = new System.Drawing.Point(193, 188);
+            this.button6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.Size = new System.Drawing.Size(100, 28);
             this.button6.TabIndex = 5;
             this.button6.Text = "Can -";
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(97, 262);
+            this.label1.Location = new System.Drawing.Point(129, 322);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.Size = new System.Drawing.Size(68, 16);
             this.label1.TabIndex = 6;
             this.label1.Text = "produttore";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(251, 329);
+            this.ClientSize = new System.Drawing.Size(335, 405);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button6);
             this.Controls.Add(this.button5);
@@ -116,6 +127,7 @@
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
--- 10-10-2022/Form1.cs
+++ 10-10-2022/Form1.cs
@@ -13,11 +13,13 @@ namespace _10_10_2022
     public partial class Form1 : Form
     {
         private Class1 tele;
-
+        int v = 0;
+        int c = 0;
         public Form1()
         {
             InitializeComponent();
             tele = new Class1();
+            
             
         }
 
@@ -29,6 +31,32 @@ namespace _10_10_2022
         private void button1_Click(object sender, EventArgs e)
         {
             tele.spegni();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            int a = v + 1;
+            
+            
+                
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int a = v-1 ;
+            
+           
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int a = c+1;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            int a = c - 1;
         }
     }
 }
